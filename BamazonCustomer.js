e88e20e7a33a82ea73569aba326905f0705630c2
edace91 <eduace91@gmail.com>
2016-10-29 14:01:56 -0500

    getting price data and stock information

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -17,5 +17,66 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    console.log("connected as id " + connection.threadId);
 })
+
+connection.query('SELECT * FROM Products', function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | $" + res[i].Price);
+    }
+    console.log("-----------------------------------");
+})
+
+var start = function() {
+	connection.query('SELECT * FROM Products', function(err, res) {
+		//console.log(res);
+    	inquirer.prompt({
+        	name: "itemId",
+        	type: "input",
+       		message: "What is the item ID you would like to purchase?"
+    	}).then(function(answer) {
+    		for (var i = 0; i < res.length; i++) {
+            	if (res[i].id == answer.itemId) {
+                	var chosenItem = res[i].id;
+                	 console.log("You have chosen item " + chosenItem);
+                	inquirer.prompt({
+                   		name: "quantity",
+                    	type: "input",
+                    	message: "How many would you like to order?"
+            		}).then(function(amount){
+                        console.log(chosenItem);
+                		if (res[chosenItem].StockQuantity > parseInt(amount.quantity)){
+
+                            var itemPrice = res[chosenItem].Price;
+
+                            console.log(itemPrice);
+
+        					//update stock ti a new variable
+       						var newStock = res[chosenItem].StockQuantity - amount.quantity;
+
+                            //query to database and change the stock amount.
+       						var updateStock = 'UPDATE Products SET StockQuantity =' + newStock + 'WHERE id =' + chosenItem;
+                            
+                            connection.query(updateStock, function(err,res){
+                                // the price of the item chosen * the answer quantity
+                                var customerTotal = amount.quantity * itemPrice;
+
+                                console.log("Order for " + amount.quantity + " was placed successfully!");
+                                console.log("Your total is " + customerTotal);
+                                start();
+
+                            }) 
+                    	} else {
+                        	console.log("Insufficient quantity! there is only " + res[chosenItem].StockQuantity + " in stock.");
+                        	
+                        	start();
+                    	} 		
+       				})
+    			}
+			}
+            //console.log(i);
+		})
+	})
+}
+start();
+ 
--- BamazonSHEMA.sql
+++ BamazonSHEMA.sql
@@ -3,7 +3,7 @@ USE Bamazon
 
 CREATE TABLE Products(
 
-	id INT NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
 	ProductName VARCHAR(100)NULL,
 	DepartmentName VARCHAR(100)NULL,
 	Price DECIMAL(10,2) NULL,
@@ -12,13 +12,32 @@ CREATE TABLE Products(
 );
 
 INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
-VALUES ('name','Krewella','Dance');
+VALUES ('Nintendo 3DS XL','Video Games','196.47','3');
 
 INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
-VALUES ('TRNDSTTR','Black Coast','Dance');
+VALUES ('Pokemon Sun','Video Games','39.96','5');
 
 INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
-VALUES ('Whos Next','The Who','Classic Rock');
+VALUES ('Harry Potter and the Cursed Child','Books','17.98','10');
 
 INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
-VALUES ('Yellow Submarine','The Beatles','Classic Rock');
+VALUES ('Joanne','Music','13.99','20');
+
+INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
+VALUES ('Bose QuietComfort 35 Wireless Headphones','Electronics','349.00','6');
+
+INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
+VALUES ('Canon EOS 5D Mark IV Full Frame Digital SLR Camera Body','Electronics','3499.00','1');
+
+INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
+VALUES ('The Collected Works of Hayao Miyazaki (Amazon Exclusive) [Blu-ray]
+','Movies','224.99','2');
+
+INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
+VALUES ('Catan 5th Edition','Toys','31.94','24');
+
+INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
+VALUES ('Apple Watch Sport,','Electronics','214.98','7');
+
+INSERT INTO Products (ProductName,DepartmentName,Price,StockQuantity) 
+VALUES ('Microsoft Surface 3 ','Electronics','449.99','11');
